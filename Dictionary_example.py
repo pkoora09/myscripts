a30ce81bbcafe81c81ebdafaa278aed0f91e7741
Pradeep Kumar Koora <PKoora@slb.com>
2019-01-29 12:47:15 +0530

    New version

---
+++ Dictionary_example.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 12:25:42 2019
+
+@author: PKoora
+"""
+
+mystr = input("Enter String:")
+mydict = {}
+
+for char in mystr:
+    temp = mystr.count(char)    
+    mydict[char] = temp
+
+print("dictionary:", mydict)
+print("occuranaces:")
+for mykey in mydict.keys():
+    print(mykey,mydict[mykey])
